ac7bc5c412dc1a2937159093274cb7bdc55c290d
Lucas Matheus <lucas0matheus0@gmail.com>
2025-01-21 12:18:14 -0300

    começo do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {LoginComponent} from './pages/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  // {
+  //   path: 'auth',
+  //   children: [
+  //     {
+  //       path: '',
+  //       redirectTo: 'login',
+  //     },
+  //     {
+  //       path: '/login',
+  //       component: LoginComponent,
+  //     }
+  //   ]
+  // }
+];
